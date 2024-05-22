f0a7e90db8bf3c5059a3a43a25afb73f21d2ee54
Luca Tacconi <luca.tacconi@monolite.net>
2024-05-23 00:37:01 +0200

    Lettura del contenuto completo dell'api

--- tools/tipi_sensore_arpa_lombardia.py
+++ tools/tipi_sensore_arpa_lombardia.py
@@ -4,17 +4,49 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-file_dest = "arpa_lombardia_sensori.csv"
+fileDestName = "arpa_lombardia_sensori.csv"
 
 try:
-    file_dest = open( os.environ.get("DATA_SRC") + '/' + file_dest , "w")
+    fileDest = open( os.environ.get("DATA_SRC") + '/' + fileDestName , "w")
 
+    f.write("Tipo Sensore\n")
 
+    limit = 1000
+    offset = 0
+    dataCheck = True
 
-    file_dest.close()
+    while dataCheck:
 
-except:
-    print("An exception occurred")
+        apiUrl = os.environ.get("ARPA_LOMBARDIA_ELENCO_SENSORI")
+        apiUrl += "?$limit=" + str(limit) + "&$offset=" + str(offset)
 
-# print(os.environ.get("ARPA_LOMBARDIA_ELENCO_SENSORI"))
+        params = {}
+        headers = {}
 
+        response = requests.get(apiUrl, params = params, headers = headers)
+
+        response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+        if response.status_code != 200:
+            raise Exception("Errore connessione al server dati.")
+
+        dataResponse = response.json()
+
+        # Verifica che data sia una lista
+        if isinstance(dataResponse, list):
+            # Conta il numero di elementi nell'array
+            numContents = len(dataResponse)
+            if numContents == 0:
+                dataCheck = False
+            else:
+                print(f"L'array contiene {numContents} elementi. {offset}")
+                offset += 1000
+
+        else:
+            dataCheck = False
+
+    print("Fine")
+
+    fileDest.close()
+
+except Exception as e:
+    print(f"Errore: {e}")
