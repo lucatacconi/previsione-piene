63684d1800a3c0f5ff7c94ddea02e042ffe87e01
Luca Tacconi <luca.tacconi@monolite.net>
2024-05-31 09:32:26 +0200

    Lettura anche dei sensori di Venezia

--- tools/elenco_sensori_arpa_veneto.py
+++ tools/elenco_sensori_arpa_veneto.py
@@ -168,6 +168,155 @@ try:
 
         elementiLetti += 1
 
+    # Sensori laguna di Venezia ========================================================================================================
+
+    posizione_sensori = {}
+
+    # Sensori livello marea
+
+    apiUrl = os.environ.get("VENEZIA_ELENCO_SENSORI_LIVELLO_MAREA")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    dataResponse = response.json()
+
+    for sensore in dataResponse:
+
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write('Livello marea' + separatore)
+        fileDest.write('m' + separatore)
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write(sensore['stazione'] + separatore)
+        fileDest.write('0' + separatore)
+        fileDest.write('VE' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write(str(sensore['latDDN']) + separatore)
+        fileDest.write(str(sensore['lonDDE']))
+        fileDest.write(fineLinea)
+
+        posizione_sensori[str(sensore['ID_stazione'])] = (str(sensore['latDDN']), str(sensore['lonDDE']))
+
+        elementiLetti += 1
+
+
+    # Sensori onda di laguna
+
+    apiUrl = os.environ.get("VENEZIA_ELENCO_SENSORI_ONDA_LAGUNA")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    dataResponse = response.json()
+
+    for sensore in dataResponse:
+
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write('Onda di laguna' + separatore)
+        fileDest.write('m' + separatore)
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write(sensore['stazione'] + separatore)
+        fileDest.write('0' + separatore)
+        fileDest.write('VE' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write(str(sensore['latDDN']) + separatore)
+        fileDest.write(str(sensore['lonDDE']))
+        fileDest.write(fineLinea)
+
+        posizione_sensori[str(sensore['ID_stazione'])] = (str(sensore['latDDN']), str(sensore['lonDDE']))
+
+        elementiLetti += 1
+
+
+    # Sensori onda fuori laguna
+
+    apiUrl = os.environ.get("VENEZIA_ELENCO_SENSORI_ONDA_MARE")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    dataResponse = response.json()
+
+    for sensore in dataResponse:
+
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write('Onda fuori laguna' + separatore)
+        fileDest.write('m' + separatore)
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write(sensore['stazione'] + separatore)
+        fileDest.write('0' + separatore)
+        fileDest.write('VE' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write(str(sensore['latDDN']) + separatore)
+        fileDest.write(str(sensore['lonDDE']))
+        fileDest.write(fineLinea)
+
+        posizione_sensori[str(sensore['ID_stazione'])] = (str(sensore['latDDN']), str(sensore['lonDDE']))
+
+        elementiLetti += 1
+
+
+    # Sensori vento
+
+    apiUrl = os.environ.get("VENEZIA_ELENCO_SENSORI_VENTO")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    dataResponse = response.json()
+
+    for sensore in dataResponse:
+
+        latDDN = ''
+        lonDDE = ''
+
+        if str(sensore['ID_stazione']) in posizione_sensori:
+            latDDN, lonDDE = posizione_sensori[str(sensore['ID_stazione'])]
+        else:
+            continue
+
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write('Direzione vento' + separatore)
+        fileDest.write('m' + separatore)
+        fileDest.write(str(sensore['ID_stazione']) + separatore)
+        fileDest.write(sensore['stazione'] + separatore)
+        fileDest.write('0' + separatore)
+        fileDest.write('VE' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write('' + separatore)
+        fileDest.write(str(latDDN) + separatore)
+        fileDest.write(str(lonDDE))
+        fileDest.write(fineLinea)
+
+        elementiLetti += 1
+
     print("Letti " + str(elementiLetti) + " dettagli sensore.")
 
     fileDest.close()
