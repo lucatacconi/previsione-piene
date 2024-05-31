86e6d5db6fd633136a86a067a94114f02c0d38af
Luca Tacconi <luca.tacconi@monolite.net>
2024-06-01 00:03:33 +0200

    Aggiunte anche le tipologie della sezione portata

--- tools/tipi_sensore_regione_emilia_rogmanga.py
+++ tools/tipi_sensore_regione_emilia_rogmanga.py
@@ -85,6 +85,52 @@ try:
         cnt_stazioni += 1
 
 
+    apiUrl = os.environ.get("ARPAE_EMILIA_ELENCO_SENSORI_PORTATA")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    data_response = response.text
+
+    if not data_response:
+        raise Exception("Errore connessione al server dati. (2)")
+
+
+    file_destinazione = open( os.environ.get("DATA_SRC") + '/' + file_destinazione_nome , "w")
+    file_destinazione.write("Tipo Sensore\n")
+
+    for stazione in data_response.split('\n'):
+
+        if stazione == "":
+            continue
+
+        json_stazione = json.loads(stazione)
+
+        if len(json_stazione['data']) == 0:
+            raise Exception("Dati stazione mancanti:" + str(cnt_stazioni))
+
+        for data in json_stazione['data']:
+
+            if not 'timerange' in data.keys() or not 'vars' in data.keys() or len(data['vars']) == 0:
+                continue
+
+            for sensore in data['vars'].keys():
+                if sensore in dizionario:
+
+                    descrizione_sensore = dizionario[sensore]["Descrizione"].upper()
+
+                    if descrizione_sensore not in tipi_letti:
+                        tipi_letti.append(descrizione_sensore)
+                        file_destinazione.write(descrizione_sensore + "\n")
+
+        cnt_stazioni += 1
+
     print("Trovati " + str(len(tipi_letti)) + " tipi di sensori.")
 
     file_destinazione.close()
