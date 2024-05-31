c7bbc4d378356afa3ae4927334d02b5e27309cad
Luca Tacconi <luca.tacconi@monolite.net>
2024-06-01 00:05:54 +0200

    Inizio della procedura di lettura dei sensori disponibili

---
+++ tools/elenco_sensori_regione_emilia_romagna.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import requests
+
+from dotenv import load_dotenv
+load_dotenv()
+
+file_destinazione_nome = "regione_emilia_romagna_sensori.csv"
+
+try:
+
+
+except Exception as e:
+    print(e)
