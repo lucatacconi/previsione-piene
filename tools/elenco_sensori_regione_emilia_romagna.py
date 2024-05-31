2cc7ffa916cb7de9586ba407f7535b5ff9cdb5eb
Luca Tacconi <luca.tacconi@monolite.net>
2024-06-01 00:10:40 +0200

    Predisposiione del file di destinazione

--- tools/elenco_sensori_regione_emilia_romagna.py
+++ tools/elenco_sensori_regione_emilia_romagna.py
@@ -6,8 +6,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 file_destinazione_nome = "regione_emilia_romagna_sensori.csv"
+separatore = ","
+fine_linea = "\n"
 
 try:
+
+    file_destinazione = open( os.environ.get("DATA_SRC") + '/' + file_destinazione_nome , "w")
+
+    #Intestazione file
+    file_destinazione.write("IDsensore" + separatore)
+    file_destinazione.write("Tipologia" + separatore)
+    file_destinazione.write("UnitaMisura" + separatore)
+    file_destinazione.write("IDStazione" + separatore)
+    file_destinazione.write("Nome" + separatore)
+    file_destinazione.write("Quota" + separatore)
+    file_destinazione.write("Provincia" + separatore)
+    file_destinazione.write("DataInstallazione" + separatore)
+    file_destinazione.write("Storico" + separatore)
+    file_destinazione.write("Latitudine" + separatore)
+    file_destinazione.write("Longitudine")
+    file_destinazione.write(fine_linea)
+
+    elementi_letti = 0
 
 
 except Exception as e:
