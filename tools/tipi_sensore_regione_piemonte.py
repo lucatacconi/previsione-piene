5996d9d0a751fd8eda73eda0a07027388ee17ee5
Luca Tacconi <luca.tacconi@monolite.net>
2024-06-13 12:58:55 +0200

    Lettura tipi di sensore Piemonte

--- tools/tipi_sensore_regione_piemonte.py
+++ tools/tipi_sensore_regione_piemonte.py
@@ -13,5 +13,32 @@ try:
 
     fileDest.write("Tipo Sensore\n")
 
+    tipiLetti = []
+
+
+    apiUrl = os.environ.get("ARPA_PIEMONTE_ELENCO_SENSORI")
+
+    params = {}
+    headers = {}
+
+    response = requests.get(apiUrl, params = params, headers = headers)
+
+    response.raise_for_status()  # Solleva un'eccezione per gli errori HTTP (4xx e 5xx)
+    if response.status_code != 200:
+        raise Exception("Errore connessione al server dati. (1)")
+
+    dataResponse = response.json()
+
+    for stazione in dataResponse['features']:
+
+        for key, sensore in stazione['properties']['sensors'].items():
+            if key not in tipiLetti:
+                tipiLetti.append(key)
+                fileDest.write(key + "\n")
+
+    print("Trovati " + str(len(tipiLetti)) + " tipi di sensori.")
+
+    fileDest.close()
+
 except Exception as e:
     print(e)
